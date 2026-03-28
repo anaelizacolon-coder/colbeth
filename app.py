973f42a87025db3acc030ab475b8016001fe127a
anaelizacolon-coder <anaelizacolon@gmail.com>
2026-03-28 13:19:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -78,6 +78,11 @@ elif choice == "Pagos y Abonos":
                 st.success("✅ Dinero registrado correctamente.")
                 st.rerun()
     
+     tab_res, tab_sal = st.tabs(["📉 Resultados Periodo", "👥 Saldos Pendientes"])
+        with tab_sal:
+            if not df_p.empty:
+            df_p['Por Cobrar'] = df_p['precio_venta'] - df_p['adelanto_cliente']
+            df_p['Por Pagar'] = df_p['costo_fabrica'] - df_p['adelanto_suplidor']
             c_cli, c_sup = st.columns(2)
             c_cli.subheader("Deudas de Clientes")
             c_cli.table(df_p[df_p['Por Cobrar'] > 0][['cliente', 'mueble', 'Por Cobrar']].style.format({"Por Cobrar": "${:,.2f}"}))
